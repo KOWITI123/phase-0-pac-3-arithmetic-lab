8ba86e97de57befdd2280755882e42617a775b64
Allan <allan.kowiti@student.moringaschool.com>
2022-08-03 17:09:29 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,56 @@
+function add() {
+    {return "1+2"}
+}
+
+function subtract() {
+    {return " 2-1"}
+}
+
+function multiply() {
+    {return "3*5"}
+}
+
+function divide() {
+    {return "20/5"}
+} 
+
+function add(a , b) {
+ {return a+b;}
+}
+console.log(sum(1,2));
+
+function subtract(a, b) {
+    {return a-b;}
+}
+console.log(subtract(10,5))
+
+function multiply(a, b) {
+    {return a*b;}
+}
+console.log(multiply(5,5))
+
+function divide(a, b) {
+    {return a/b}
+}
+console.log(divide(a,b))
+
+ function increment(n) {
+    {return ++n}
+}
+console.log(increment(2))
+
+function decrement(n) {
+    {return --n}
+}
+console.log(decrement(5))
+
+function makeInt(n) {
+    return parseInt(n,10)
+}
+console.log(makeInt(0))
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
+console.log(preserveDecimal(2.222))
+
